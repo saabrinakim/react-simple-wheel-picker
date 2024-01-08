5ad364e81805e9ae00c4cbc56660f95706c9d22f
Sabrina Kim <saabrinakim@gmail.com>
2024-01-08 17:49:40 -0300

    Fix build errors

--- src/components/WheelPicker.tsx
+++ src/components/WheelPicker.tsx
@@ -1,10 +1,10 @@
 import React, {
-  useMemo,
-  useImperativeHandle,
-  forwardRef,
-  useState,
-  useEffect,
-  useCallback
+	useMemo,
+	useImperativeHandle,
+	forwardRef,
+	useState,
+	useEffect,
+	useCallback,
 } from "react";
 import styled from "styled-components";
 import WheelPickerItem from "../components/WheelPickerItem";
@@ -13,23 +13,23 @@ import { OPTION_ID } from "../constants/optionID";
 import useHandleKeyboard from "../hooks/useHandleKeyboard";
 
 const List = styled.ul`
-  position: relative;
-  margin: 0;
-  padding: 0;
-  display: inline-block;
-  list-style: none;
-  overflow-y: scroll;
-  will-change: transform;
-  overflow-x: hidden;
-  text-align: center;
-  padding: 0 20px;
-  ${(props: {
-    height: number;
-    width: string;
-    backgroundColor: string;
-    shadowColor: string;
-    focusColor: string;
-  }): string => `
+	position: relative;
+	margin: 0;
+	padding: 0;
+	display: inline-block;
+	list-style: none;
+	overflow-y: scroll;
+	will-change: transform;
+	overflow-x: hidden;
+	text-align: center;
+	padding: 0 20px;
+	${(props: {
+		height: number;
+		width: string;
+		backgroundColor: string;
+		shadowColor: string;
+		focusColor: string;
+	}): string => `
     height: ${props.height}px;
     width: ${props.width};
     background-color: ${props.backgroundColor};
@@ -41,197 +41,197 @@ const List = styled.ul`
 `;
 
 const calculateSpaceHeight = (height: number, itemHeight: number): number => {
-  const limit = height / itemHeight / 2 - 0.5;
-  return itemHeight * limit;
+	const limit = height / itemHeight / 2 - 0.5;
+	return itemHeight * limit;
 };
 
 const setStyles = (styles: {
-  width?: number;
-  color?: string;
-  activeColor?: string;
-  fontSize?: number;
-  backgroundColor?: string;
-  shadowColor?: string;
-  focusColor?: string;
+	width?: number;
+	color?: string;
+	activeColor?: string;
+	fontSize?: number;
+	backgroundColor?: string;
+	shadowColor?: string;
+	focusColor?: string;
 }) => {
-  const _color = styles.color || "#fff";
-  return {
-    color: _color,
-    activeColor: styles.activeColor || _color,
-    fontSize: styles.fontSize || 16,
-    backgroundColor: styles.backgroundColor || "#555",
-    shadowColor: styles.shadowColor || "#333",
-    width: styles.width ? `${styles.width}px` : "100%",
-    focusColor: styles.focusColor ? styles.focusColor : "blue"
-  };
+	const _color = styles.color || "#fff";
+	return {
+		color: _color,
+		activeColor: styles.activeColor || _color,
+		fontSize: styles.fontSize || 16,
+		backgroundColor: styles.backgroundColor || "#555",
+		shadowColor: styles.shadowColor || "#333",
+		width: styles.width ? `${styles.width}px` : "100%",
+		focusColor: styles.focusColor ? styles.focusColor : "blue",
+	};
 };
 
 export interface PickerData {
-  id: string;
-  value: string | number;
+	id: string;
+	value: string | number;
 }
 
 export interface WheelPickerProps {
-  data: PickerData[];
-  selectedID: string;
-  onChange: (data: PickerData) => void;
-  height: number;
-  itemHeight: number;
-  idName?: string;
-  titleID?: string;
-  titleText?: string;
-  required?: boolean;
-  width?: number;
-  color?: string;
-  activeColor?: string;
-  fontSize?: number;
-  backgroundColor?: string;
-  shadowColor?: string;
-  focusColor?: string;
+	data: PickerData[];
+	selectedID: string;
+	onChange: (data: PickerData) => void;
+	height: number;
+	itemHeight: number;
+	idName?: string;
+	titleID?: string;
+	titleText?: string;
+	required?: boolean;
+	width?: number;
+	color?: string;
+	activeColor?: string;
+	fontSize?: number;
+	backgroundColor?: string;
+	shadowColor?: string;
+	focusColor?: string;
 }
 
 const WheelPicker: React.FC<WheelPickerProps> = (
-  {
-    data,
-    selectedID,
-    onChange,
-    height,
-    itemHeight,
-    idName,
-    titleID,
-    titleText,
-    width,
-    color,
-    activeColor,
-    fontSize,
-    backgroundColor,
-    shadowColor,
-    focusColor,
-    required
-  },
-  ref
+	{
+		data,
+		selectedID,
+		onChange,
+		height,
+		itemHeight,
+		idName,
+		titleID,
+		titleText,
+		width,
+		color,
+		activeColor,
+		fontSize,
+		backgroundColor,
+		shadowColor,
+		focusColor,
+		required,
+	},
+	ref
 ) => {
-  const [_itemHeight, setItemHeight] = useState(itemHeight);
-  const { onKeyUp, onKeyPress } = useHandleKeyboard(_itemHeight);
-  const { root, refs, activeID, onFocus } = useObserver(
-    data,
-    selectedID,
-    _itemHeight,
-    onChange
-  );
-  const styles = useMemo(
-    () =>
-      setStyles({
-        width,
-        color,
-        activeColor,
-        fontSize,
-        backgroundColor,
-        shadowColor,
-        focusColor
-      }),
-    [
-      activeColor,
-      backgroundColor,
-      color,
-      focusColor,
-      fontSize,
-      shadowColor,
-      width
-    ]
-  );
+	const [_itemHeight, setItemHeight] = useState(itemHeight);
+	const { onKeyUp, onKeyPress } = useHandleKeyboard(_itemHeight);
+	const { root, refs, activeID, onFocus } = useObserver(
+		data,
+		selectedID,
+		_itemHeight,
+		onChange
+	);
+	const styles = useMemo(
+		() =>
+			setStyles({
+				width,
+				color,
+				activeColor,
+				fontSize,
+				backgroundColor,
+				shadowColor,
+				focusColor,
+			}),
+		[
+			activeColor,
+			backgroundColor,
+			color,
+			focusColor,
+			fontSize,
+			shadowColor,
+			width,
+		]
+	);
 
-  const spaceHeight = useMemo(() => calculateSpaceHeight(height, _itemHeight), [
-    _itemHeight,
-    height
-  ]);
+	const spaceHeight = useMemo(() => calculateSpaceHeight(height, _itemHeight), [
+		_itemHeight,
+		height,
+	]);
 
-  const ariaActivedescendant = useMemo(() => {
-    return `${OPTION_ID}${activeID}`;
-  }, [activeID]);
+	const ariaActivedescendant = useMemo(() => {
+		return `${OPTION_ID}${activeID}`;
+	}, [activeID]);
 
-  const handleOnClick = useCallback(
-    (e: React.MouseEvent<HTMLLIElement>) => {
-      if (root.current) {
-        root.current.scrollTo(0, e.currentTarget.offsetTop - spaceHeight);
-      }
-    },
-    [root, spaceHeight]
-  );
+	const handleOnClick = useCallback(
+		(e: React.MouseEvent<HTMLLIElement>) => {
+			if (root.current) {
+				root.current.scrollTo(0, e.currentTarget.offsetTop - spaceHeight);
+			}
+		},
+		[root, spaceHeight]
+	);
 
-  useImperativeHandle(
-    ref,
-    () => ({
-      focus: () => {
-        root.current && root.current.focus();
-      },
-      blur: () => {
-        root.current && root.current.blur();
-      }
-    }),
-    [root]
-  );
+	useImperativeHandle(
+		ref,
+		() => ({
+			focus: () => {
+				root.current && root.current.focus();
+			},
+			blur: () => {
+				root.current && root.current.blur();
+			},
+		}),
+		[root]
+	);
 
-  useEffect(() => {
-    const adjustItemHeight = () => {
-      let maxHeight = itemHeight;
-      Object.keys(refs).map(id => {
-        const elm = refs[id].current;
-        if (!elm) {
-          return;
-        }
+	useEffect(() => {
+		const adjustItemHeight = () => {
+			let maxHeight = itemHeight;
+			Object.keys(refs).map((id) => {
+				const elm = refs[id].current;
+				if (!elm) {
+					return;
+				}
 
-        const h = elm.clientHeight;
-        if (h > maxHeight) {
-          maxHeight = h;
-        }
-      });
-      return maxHeight;
-    };
-    setItemHeight(adjustItemHeight());
-  }, [itemHeight, refs]);
+				const h = elm.clientHeight;
+				if (h > maxHeight) {
+					maxHeight = h;
+				}
+			});
+			return maxHeight;
+		};
+		setItemHeight(adjustItemHeight());
+	}, [itemHeight, refs]);
 
-  return (
-    <List
-      id={idName}
-      tabIndex={0}
-      role="listbox"
-      aria-labelledby={titleID}
-      aria-label={titleText}
-      aria-required={required}
-      aria-activedescendant={ariaActivedescendant}
-      ref={root}
-      data-testid="picker-list"
-      height={height}
-      width={styles.width}
-      backgroundColor={styles.backgroundColor}
-      shadowColor={styles.shadowColor}
-      focusColor={styles.focusColor}
-      onKeyUp={onKeyUp}
-      onKeyPress={onKeyPress}
-      onKeyDown={onKeyPress}
-    >
-      <div style={{ height: spaceHeight }} />
-      {data.map(item => (
-        <WheelPickerItem
-          key={item.id}
-          {...item}
-          {...styles}
-          height={_itemHeight}
-          activeID={activeID}
-          onClick={handleOnClick}
-          onFocus={onFocus}
-          ref={refs[item.id]}
-        />
-      ))}
-      <div style={{ height: spaceHeight }} />
-    </List>
-  );
+	return (
+		<List
+			id={idName}
+			tabIndex={0}
+			role="listbox"
+			aria-labelledby={titleID}
+			aria-label={titleText}
+			aria-required={required}
+			aria-activedescendant={ariaActivedescendant}
+			ref={root}
+			data-testid="picker-list"
+			height={height}
+			width={styles.width}
+			backgroundColor={styles.backgroundColor}
+			shadowColor={styles.shadowColor}
+			focusColor={styles.focusColor}
+			onKeyUp={onKeyUp}
+			onKeyPress={onKeyPress}
+			onKeyDown={onKeyPress}
+		>
+			<div style={{ height: spaceHeight }} />
+			{data.map((item) => (
+				<WheelPickerItem
+					key={item.id}
+					{...item}
+					{...styles}
+					height={_itemHeight}
+					activeID={activeID}
+					onClick={handleOnClick}
+					onFocus={onFocus}
+					ref={refs[item.id]}
+				/>
+			))}
+			<div style={{ height: spaceHeight }} />
+		</List>
+	);
 };
 
 export interface WheelPickerRef {
-  focus: () => void;
-  blur: () => void;
+	focus: () => void;
+	blur: () => void;
 }
 
 export default forwardRef<WheelPickerRef, WheelPickerProps>(WheelPicker);
--- src/components/WheelPickerItem.tsx
+++ src/components/WheelPickerItem.tsx
@@ -1,118 +1,92 @@
 import React, {
-  forwardRef,
-  useMemo,
-  FocusEventHandler,
-  MouseEventHandler
+	forwardRef,
+	useMemo,
+	FocusEventHandler,
+	MouseEventHandler,
 } from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { OPTION_ID } from "../constants/optionID";
 
 const Item = styled.li`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: left;
-  cursor: pointer;
-  ${(props: { height: number }): string => `
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: left;
+	cursor: pointer;
+	${(props: { height: number }): string => `
     min-height: ${props.height}px;
   `}
-  &:focus {
-    outline: none;
-  }
-`;
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
+	&:focus {
+		outline: none;
+	}
 `;
 
 const ICON_WIDTH = 20;
-const Icon = styled.span`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: -10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: ${ICON_WIDTH}px;
-  opacity: 0;
-  animation: ${fadeIn} 200ms ease-in;
-  animation-fill-mode: forwards;
-  animation-delay: 200ms;
-  ${(props: { fontSize: number }) => `
-    font-size: ${props.fontSize};
-  `}
-`;
 
 const Text = styled.p`
-  margin: 0;
-  text-align: left;
-  word-wrap: break-word;
-  padding-left: 10px;
+	margin: 0;
+	text-align: left;
+	word-wrap: break-word;
+	padding-left: 10px;
 `;
 
 export interface WheelPickerItemProps {
-  id: string;
-  value: string | number;
-  activeID: string;
-  height: number;
-  color: string;
-  activeColor: string;
-  fontSize: number;
-  onClick?: MouseEventHandler;
-  onFocus?: FocusEventHandler;
+	id: string;
+	value: string | number;
+	activeID: string;
+	height: number;
+	color: string;
+	activeColor: string;
+	fontSize: number;
+	onClick?: MouseEventHandler;
+	onFocus?: FocusEventHandler;
 }
 
 const WheelPickerItem: React.FC<WheelPickerItemProps> = (
-  {
-    id,
-    value,
-    activeID,
-    height,
-    color,
-    activeColor,
-    fontSize,
-    onClick,
-    onFocus
-  },
-  ref
+	{
+		id,
+		value,
+		activeID,
+		height,
+		color,
+		activeColor,
+		fontSize,
+		onClick,
+		onFocus,
+	},
+	ref
 ) => {
-  const selected = useMemo(() => id === activeID, [id, activeID]);
-  const textColor = useMemo(() => (selected ? activeColor : color), [
-    activeColor,
-    color,
-    selected
-  ]);
-  const textStyle = useMemo(
-    () => ({
-      color: textColor,
-      fontSize
-    }),
-    [fontSize, textColor]
-  );
-  return (
-    <Item
-      role="option"
-      aria-selected={selected}
-      aria-label={value.toString()}
-      ref={ref}
-      id={`${OPTION_ID}${id}`}
-      data-itemid={id}
-      data-itemvalue={value}
-      height={height}
-      onClick={onClick}
-      onFocus={onFocus}
-      tabIndex={0}
-    >
-      <span style={{ width: ICON_WIDTH }}></span>
-      <Text style={textStyle}>{value}</Text>
-    </Item>
-  );
+	const selected = useMemo(() => id === activeID, [id, activeID]);
+	const textColor = useMemo(() => (selected ? activeColor : color), [
+		activeColor,
+		color,
+		selected,
+	]);
+	const textStyle = useMemo(
+		() => ({
+			color: textColor,
+			fontSize,
+		}),
+		[fontSize, textColor]
+	);
+	return (
+		<Item
+			role="option"
+			aria-selected={selected}
+			aria-label={value.toString()}
+			ref={ref}
+			id={`${OPTION_ID}${id}`}
+			data-itemid={id}
+			data-itemvalue={value}
+			height={height}
+			onClick={onClick}
+			onFocus={onFocus}
+			tabIndex={0}
+		>
+			<span style={{ width: ICON_WIDTH }}></span>
+			<Text style={textStyle}>{value}</Text>
+		</Item>
+	);
 };
 
 export default forwardRef<HTMLLIElement, WheelPickerItemProps>(WheelPickerItem);
